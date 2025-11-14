c1e41bf693c5288b0adafa3b1c0f32feb099a3a6
Sajal Bansal <sajalbnl123@gmail.com>
2025-11-14 22:37:48 +0530

    platform android some issue fixed

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -22,7 +22,9 @@ import {
   useLoginWithOAuth,
 } from '@privy-io/expo';
 
-
+if (Platform.OS === 'android') {
+  WebBrowser.warmUpAsync();
+}
 
 const AuthScreen = () => {
   const router = useRouter();
